171fc6ec061f33299ca9ec1f67c584bd303e9a08
Kaapo Seppänen <kaapo1996@gmail.com>
2023-11-27 23:00:22 +0200

    Completed assignment

--- data/create_human.R
+++ data/create_human.R
@@ -2,8 +2,11 @@
 # Date: November 27, 2023
 # Description: Pre-processing of two data sets from https://raw.githubusercontent.com/KimmoVehkalahti/Helsinki-Open-Data-Science/master/datasets
 
-# reading the two data sets
+# load the required libraries
 library(readr)
+library(dplyr)
+
+# reading the two data sets
 hd <- read_csv("https://raw.githubusercontent.com/KimmoVehkalahti/Helsinki-Open-Data-Science/master/datasets/human_development.csv")
 gii <- read_csv("https://raw.githubusercontent.com/KimmoVehkalahti/Helsinki-Open-Data-Science/master/datasets/gender_inequality.csv", na = "..")
 
@@ -15,5 +18,24 @@ str(gii)
 summary(hd)
 summary(gii)
 
-colnames(hd)
-colnames(gii)
+# rename variables
+names(hd) <- c("HDI.Rank", "Country", "HDI", "Life.Exp", "Edu.Exp", "Edu.Mean", "GNI", "GNI.Minus.Rank")
+names(gii) <- c("GII.Rank", "Country", "GII", "Mat.Mor", "Ado.Birth", "Parli.F", "Edu2.F", "Edu2.M", "Labo.F", "Labo.M")
+
+# create a variable for the ratio of female and male populations with secondary education in each country
+gii <- mutate(gii, Edu2.FM = Edu2.F / Edu2.M)
+
+# create a variable for the ratio of labor force participation of females and males in each country
+gii <- mutate(gii, Labo.FM = Labo.F / Labo.M)
+
+# join the two data sets by country
+human <- inner_join(hd, gii, by = "Country")
+
+# print out the summary of the joined data set
+str(human)
+
+# write data to file
+write_csv(human, "data/human.csv")
+
+
+
