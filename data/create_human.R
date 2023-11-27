cee8e316162af50f5172e6f3ddaadc4967441dba
Kaapo Seppänen <kaapo1996@gmail.com>
2023-11-27 17:12:04 +0200

    Created file

---
+++ data/create_human.R
@@ -0,0 +1,7 @@
+# Author: Kaapo Seppänen
+# Date: November 27, 2023
+# Description: Pre-processing of two data sets from https://raw.githubusercontent.com/KimmoVehkalahti/Helsinki-Open-Data-Science/master/datasets
+
+
+
+
