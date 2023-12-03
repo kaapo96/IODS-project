134a4f917bee886360d37fd7a0077dc5e9a5de42
Kaapo Seppänen <kaapo1996@gmail.com>
2023-12-03 14:28:12 +0200

    Created file

---
+++ data/create_human2.R
@@ -0,0 +1,12 @@
+# Author: Kaapo Seppänen
+# Date: December 3, 2023
+# Description: Pre-processing of two data sets from https://raw.githubusercontent.com/KimmoVehkalahti/Helsinki-Open-Data-Science/master/datasets
+
+# load the required libraries
+library(readr)
+library(dplyr)
+
+
+
+
+
