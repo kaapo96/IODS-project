19c1197bd46766cb209d1cab19e981640d318959
Kaapo Seppänen <kaapo1996@gmail.com>
2023-12-03 15:08:01 +0200

    Continued assignment

--- data/create_human2.R
+++ data/create_human2.R
@@ -1,12 +1,36 @@
 # Author: Kaapo Seppänen
 # Date: December 3, 2023
-# Description: Pre-processing of two data sets from https://raw.githubusercontent.com/KimmoVehkalahti/Helsinki-Open-Data-Science/master/datasets
+# Description: Continues the pre-processing of two data sets from https://raw.githubusercontent.com/KimmoVehkalahti/Helsinki-Open-Data-Science/master/datasets
 
 # load the required libraries
 library(readr)
 library(dplyr)
 
+# load the data set
+human <- read_csv("data/human.csv")
 
+# print out structure and dimensions
+str(human)
 
+# Variables:
+# HDI.rank: 
+# Country: 
+# HDI: 
+# Life.Exp: 
+# Edu.Exp: 
+# Edu.Mean: 
+# GNI: 
+# GNI.Minus.Rank: 
+# GII.Rank: 
+# GII: 
+# Mat.Mor: 
+# Ado.Birth: 
+# Parli.F: 
+# Edu2.F: 
+# Edu2.M: 
+# Labo.F: 
+# Labo.M: 
+# Edu2.FM: 
+# Labo.FM: 
 
 
